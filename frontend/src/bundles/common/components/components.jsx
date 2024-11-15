f699280e862875346499273c9963da6a3ddfa89b
XCODE89 <maph0989@gmail.com>
2024-11-15 19:43:23 -0300

    TM-1: create-frontend-template

---
+++ frontend/src/bundles/common/components/components.jsx
@@ -0,0 +1,10 @@
+export { Navigate, Outlet as RouterOutlet } from 'react-router-dom';
+export { ProtectedRoute } from './protected-route/protected-route.jsx';
+export { RouterProvider } from './router-provider/router-provider.jsx';
+
+export {
+    Button,
+    Link,
+    Typography,
+} from '@mui/material';
+
