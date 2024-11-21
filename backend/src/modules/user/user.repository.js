b86477f4439885b39a8438920a53091dca87337f
XCODE89 <maph0989@gmail.com>
2024-11-21 10:55:35 -0300

    TM-14: + sign-up-logic

--- backend/src/modules/user/user.repository.js
+++ backend/src/modules/user/user.repository.js
@@ -3,29 +3,12 @@ class UserRepository {
       this.userModel = userModel;
   }
 
-  async findAll() {
-    return await this.userModel.findAll(); // Obtiene todos los usuarios
-  }
-
-  async findById(id) {
-      return await this.userModel.findByPk(id);
-  }
-
   async findByEmail(email) {
       return await this.userModel.findOne({ where: { email } });
   }
 
   async create(userData) {
       return await this.userModel.create(userData);
-  }
-
-  async update(id, updatedData) {
-      await this.userModel.update(updatedData, { where: { id } });
-      return await this.findById(id);
-  }
-
-  async delete(id) {
-      return await this.userModel.destroy({ where: { id } });
   }
 }
 
