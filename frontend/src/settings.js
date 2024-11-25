bed0b4da198e36948078ace26e2c5b52912a18c7
CamilaElizabetMartinez <martinezcamila073@gmail.com>
2024-11-25 17:50:39 +0100

    fix: implemented enpoint of register

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL = "http://localhost:3000/";
+export const API_BASE_URL = "http://localhost:3001/";
 
-export const SIGN_UP = API_BASE_URL + "";
+export const SIGN_UP = API_BASE_URL + "api/user/register";
 
 export const ENDPOINTS = {
   SIGN_UP,
