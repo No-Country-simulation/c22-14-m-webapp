9a034123c50bab426d7234786d3e6f2074ca9b22
CamilaElizabetMartinez <martinezcamila073@gmail.com>
2024-11-27 14:02:02 +0100

    task: Implemented imports

--- frontend/src/bundles/auth/pages/auth.jsx
+++ frontend/src/bundles/auth/pages/auth.jsx
@@ -1,4 +1,7 @@
-import { Button, Link } from '../../common/components/components';
+import React, { useState, useEffect } from 'react';
+import { Button, Link, Container, TextField, Typography, Box } from '../../common/components/components';
+import { Navigate } from 'react-router-dom';
+import { SIGN_IN } from '../../../settings';
 
 const Auth = () => {
   return (
