072dba26f58b7e15a1357234e2de976d070451bf
CamilaElizabetMartinez <martinezcamila073@gmail.com>
2024-11-27 13:56:00 +0100

    task: Create endpoint to login

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,7 +1,9 @@
 export const API_BASE_URL = "http://localhost:3001/";
 
 export const SIGN_UP = API_BASE_URL + "api/user/register";
+export const SIGN_IN = API_BASE_URL + "api/user/login";
 
 export const ENDPOINTS = {
   SIGN_UP,
+  SIGN_IN,
 };
