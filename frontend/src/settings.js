55961277bfa656e5bd473deee42250a999f4ada9
naylapalaoro <nayluclash@gmail.com>
2024-12-09 09:59:52 -0300

    agrege el endpoin en FEAT_APPOIMENT

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -2,7 +2,7 @@ export const API_BASE_URL = "http://localhost:3001/";
 
 export const SIGN_UP = API_BASE_URL + "api/user/register";
 export const SIGN_IN = API_BASE_URL + "api/user/login";
-export const FEAT_APPOIMENT = API_BASE_URL + "api";
+export const FEAT_APPOIMENT = API_BASE_URL + "api/appointments/schedule";
 export const HISTORY_MEDICAL = API_BASE_URL + "api/medicalHistory/:appoiment_id";
 
 
