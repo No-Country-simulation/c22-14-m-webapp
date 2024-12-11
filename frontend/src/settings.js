1c906cbb2ff3c34fd8dee00e257e5c756b48160f
naylapalaoro <nayluclash@gmail.com>
2024-12-10 23:07:07 -0300

    marcha atras

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -5,7 +5,7 @@ export const SIGN_IN = API_BASE_URL + "api/user/login";
 export const FEAT_APPOIMENT = API_BASE_URL + "api/appointments/schedule";
 export const HISTORY_MEDICAL = API_BASE_URL + "api/medicalHistory/:appoiment_id";
 export const APPOIMENT_MANAGEMENT = API_BASE_URL + "api/"; // ruta de jaime para traer los appoinments
-export const DOCTOR_SELECTION = API_BASE_URL + "api/appointments/doctorsEsp"; 
+export const DOCTOR_SELECTION = API_BASE_URL + "api/"; //ruta de jaime para traer los doctores
 
 
 export const ENDPOINTS = {
